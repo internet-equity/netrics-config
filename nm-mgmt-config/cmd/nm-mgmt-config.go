3c723fe4478c51234142d51871c3e3d144b30467
ggmartins <ggmartins@gmail.com>
2023-06-09 21:45:08 +0000

    fix: new etccron

--- nm-mgmt-config/cmd/nm-mgmt-config.go
+++ nm-mgmt-config/cmd/nm-mgmt-config.go
@@ -10,9 +10,12 @@ import (
 	"github.com/BurntSushi/toml"
 )
 
+const version = "0.1.1"
+
 var netrics_v1_config = "https://netrics-config1.s3.amazonaws.com/%s/nm-exp-active-netrics.toml"
 var netrics_v1_etcpath = "/etc/nm-exp-active-netrics/"
-var netrics_v1_etccron = "/etc/cron.d/"
+//var netrics_v1_etccron = "/etc/cron.d/"
+var netrics_v1_etccron = "/etc/nm-exp-active-netrics/"
 var devid = ""
 
 func writable(path string) bool {
@@ -24,10 +27,10 @@ func extract_crond(conf_output string, netrics_v1_etccron string) {
                                             "cron-nm-exp-active-netrics")
         var Conf map[string]any
 	data, err := os.ReadFile(conf_output)
-    	if err != nil {
-        	fmt.Printf("ERROR: reading (%s).", conf_output)
+	if err != nil {
+		fmt.Printf("ERROR: reading (%s).", conf_output)
 		os.Exit(1)
-    	}
+	}
 	err=toml.Unmarshal(data, &Conf)
         for k, v := range Conf {
 		if k == "cron" {
@@ -48,26 +51,26 @@ func main() {
         if len(devid) > 0 {
 		fmt.Printf("INFO: My ID:%s\n", devid)
 
-   		response, error := http.Get(fmt.Sprintf(netrics_v1_config, devid))
+		response, error := http.Get(fmt.Sprintf(netrics_v1_config, devid))
 		if error != nil {
-      			fmt.Printf("ERROR: get (%s)", error)
+			fmt.Printf("ERROR: get (%s)", error)
 			os.Exit(1)
-   		}
+		}
 
 		body, error := ioutil.ReadAll(response.Body)
-   		if error != nil {
-      			fmt.Printf("ERROR: ioread (%s)", error)
+		if error != nil {
+			fmt.Printf("ERROR: ioread (%s)", error)
 			os.Exit(1)
-   		}
-   		response.Body.Close()
+		}
+		response.Body.Close()
 
 		if writable(netrics_v1_etcpath) {
 			var conf_output = filepath.Join(netrics_v1_etcpath,
                                               "nm-exp-active-netrics.toml")
 			error := os.WriteFile(conf_output, body, 0644)
 			if error != nil {
-                        	fmt.Printf("ERROR: write config (%s)", error)
-                        	os.Exit(1)
+				fmt.Printf("ERROR: write config (%s)", error)
+				os.Exit(1)
 			}
 			extract_crond(conf_output, netrics_v1_etccron)
 		} else {
